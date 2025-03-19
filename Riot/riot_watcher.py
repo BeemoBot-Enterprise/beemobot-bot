65e9213310e99cf16f88c64d5caf930777552ad5
Louis <l.cournollet@gmail.Com>
2025-03-19 15:21:34 +0100

    création comande /user avec image et embed corrrectement formaté

--- Riot/riot_watcher.py
+++ Riot/riot_watcher.py
@@ -2,6 +2,7 @@ import os
 from riotwatcher import LolWatcher, RiotWatcher, ApiError
 from colorama import Fore, Back, Style
 from toolbox import *
+from Riot.riot_toolbox import *
 
 RIOT_API_KEY = os.getenv("RIOT_API_KEY")
 DEFAULT_REGION = 'euw1'
@@ -10,23 +11,34 @@ DEFAULT_API_REGION = 'EUROPE'
 lol_watcher = LolWatcher(RIOT_API_KEY)
 riot_watcher = RiotWatcher(RIOT_API_KEY)
 
-def get_puuid_by_name(summoner_name):
-    summoner_name = split_summoner_name_and_tag(summoner_name)
-    name = summoner_name[0]
-    tag = summoner_name[1]
+# ================================================================================================
+# ====================================== User Infos ==============================================
+# ================================================================================================
+
+def get_puuid_by_name_and_tag(name, tag):
+    print("get_puuid_by_name_tag_and_region")
     return riot_watcher.account.by_riot_id(DEFAULT_API_REGION, name, tag)["puuid"]
 
-def get_id_by_puuid(region, puuid):
+def get_id_by_puuid_and_region(puuid, region):
+    print("get_id_by_puuid_and_region")
     return lol_watcher.summoner.by_puuid(region, puuid)["id"]
 
-def get_rank_by_summoner_id(region, id):
+
+def get_user_id_by_name_tag_and_region(name, tag, region):
+    print("get_user_id_by_name_tag_and_region")
+    return get_id_by_puuid_and_region(get_puuid_by_name_and_tag(name, tag), region)
+# ================================================================================================
+def get_user_icon_id_by_user_id_and_region(id, region):
+    print("get_user_icon_id_by_user_id_and_region")
+    return lol_watcher.summoner.by_id(region, id)["profileIconId"]
+
+def get_rank_by_id_and_region(id, region):
+    print("get_rank_by_id_and_region")
     return lol_watcher.league.by_summoner(region, id)
 
-def get_icon_by_iconId(iconId):
-    return f"http://ddragon.leagueoflegends.com/cdn/{get_game_version()['profileicon']}/img/profileicon/{iconId}.png"
+def slash_user(name, tag, region):
+    print("slash_user")
+    return get_icon_by_iconId(get_user_icon_id_by_user_id_and_region(get_user_id_by_name_tag_and_region(name, tag, region), region), lol_watcher, DEFAULT_REGION)
 
-def get_game_version():
-    return lol_watcher.data_dragon.versions_for_region(DEFAULT_REGION)["n"]
-
-print(get_game_version())
-print(get_rank_by_summoner_id(DEFAULT_REGION, get_id_by_puuid(DEFAULT_REGION, get_puuid_by_name("Nunch#N7789"))))
+# print('rank :')
+# print(get_rank_by_id_and_region(get_user_id_by_name_tag_and_region("MFF电竞 VX WTWTTC", "LPL整队", "euw1"), "euw1"))
